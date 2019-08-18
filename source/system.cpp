1247c4ea856266b3c808246be320503d7d914171
U. Bruhin <urbibruhin@bluewin.ch>
2019-08-18 23:32:42 +0200

    Use PLL and increase system clock to 32MHz

--- source/system.cpp
+++ source/system.cpp
@@ -12,6 +12,29 @@ System::System() {
     ;
   Watchdog::enable();
 
+  // Enable HSI
+  LL_RCC_HSI_Enable();
+  while (!LL_RCC_HSI_IsReady())
+    ;
+
+  // Set flash wait states
+  FLASH->ACR |= FLASH_ACR_LATENCY;
+
+  // Enable PLL: 16MHz -> 64MHz -> 32MHz
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLL_MUL_4,
+                              LL_RCC_PLL_DIV_2);
+  LL_RCC_PLL_Enable();
+  while (!LL_RCC_PLL_IsReady())
+    ;
+
+  // Use PLL as system clock
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+    ;
+
+  // Disable MSI
+  LL_RCC_MSI_Disable();
+
   // Enable GPIO clocks
   RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
   RCC->IOPENR |= RCC_IOPENR_GPIOBEN;
@@ -30,7 +53,7 @@ System::System() {
 #pragma GCC optimize("O0")
 void        System::delay(uint32_t ms) {
   for (volatile uint32_t i = 0U; i < ms; i++) {
-    for (volatile uint32_t k = 0U; k < 130U; ++k)
+    for (volatile uint32_t k = 0U; k < 1300U; ++k)
       ;
   }
 }
